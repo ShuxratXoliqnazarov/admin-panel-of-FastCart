9be8268cbdffff045bfbdfbeeb1eab576b982d7d
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2025-07-04 13:46:18 +0300

    adding add function for brands and function delete for products , brands and category

--- src/pages/brands/brands.jsx
+++ src/pages/brands/brands.jsx
@@ -1,8 +1,35 @@
-import { Button } from '@mui/material'
+import { Box, Button, Dialog, DialogTitle, TextField } from '@mui/material'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
 import './brands.css'
 import { Link } from 'react-router-dom'
+import { useBrandStore } from '../../store/brandStore'
+import { useEffect, useState } from 'react'
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined'
 
 export default function Brands() {
+	const { brands, getBrands, addBrand, delBrand } = useBrandStore()
+
+	const [addModal, setAddModal] = useState(false)
+	const [brandName, setBrandName] = useState('')
+
+	const handleClose = () => {
+		onClose(selectedValue)
+	}
+
+	function handleAdd() {
+		let brand = {
+			BrandName: brandName,
+		}
+
+		addBrand(brand)
+		setBrandName('')
+		setAddModal(false)
+	}
+
+	useEffect(() => {
+		getBrands()
+	}, [])
+
 	return (
 		<>
 			<div
@@ -26,9 +53,62 @@ export default function Brands() {
 				</div>
 
 				<div>
-					<Button variant='contained'>Add New</Button>
+					<Button variant='contained' onClick={() => setAddModal(true)}>
+						Add New
+					</Button>
 				</div>
 			</div>
+
+			<section className='brandsSec'>
+				{brands?.map(el => (
+					<article className='brand' key={el.id}>
+						<h1>{el.brandName}</h1>
+						<div>
+							<Button>
+								<BorderColorOutlinedIcon />
+							</Button>
+							<Button color='error' onClick={() => delBrand(el.id)}>
+								<DeleteOutlineIcon />
+							</Button>
+						</div>
+					</article>
+				))}
+			</section>
+
+			<Dialog onClose={handleClose} open={addModal}>
+				<DialogTitle>Add New Category</DialogTitle>
+				<Box
+					style={{
+						padding: 20,
+						display: 'flex',
+						flexDirection: 'column',
+						gap: 20,
+						alignItems: 'end',
+					}}
+				>
+					<TextField
+						id='outlined-basic'
+						label='Category name'
+						variant='outlined'
+						value={brandName}
+						onChange={e => setBrandName(e.target.value)}
+						sx={{ width: '100%' }}
+					/>
+
+					<div style={{ display: 'flex', gap: '20px' }}>
+						<Button
+							color='error'
+							variant='text'
+							onClick={() => setAddModal(false)}
+						>
+							Cancel
+						</Button>
+						<Button color='' variant='contained' onClick={handleAdd}>
+							Save
+						</Button>
+					</div>
+				</Box>
+			</Dialog>
 		</>
 	)
 }
---
+++ src/store/brandStore.js
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import { axiosRequest, axiosStandart } from '../utils/axios'
+
+export const useBrandStore = create((set, get) => ({
+	brands: [],
+	getBrands: async () => {
+		try {
+			let { data } = await axiosStandart.get('Brand/get-brands')
+			console.log('BRANDS: ', data.data)
+			set(() => ({ brands: data.data }))
+		} catch (error) {
+			console.log(error)
+		}
+	},
+
+	addBrand: async brand => {
+		try {
+			await axiosRequest.post(`Brand/add-brand?BrandName=${brand.BrandName}`)
+			get().getBrands()
+		} catch (error) {
+			console.log(error)
+		}
+	},
+
+	delBrand: async id => {
+		try {
+			await axiosRequest.delete(`Brand/delete-brand?id=${id}`)
+			get().getBrands()
+		} catch (error) {
+			console.log(error)
+		}
+	},
+}))
