ce06be4e9c0c7ee0a565a1623bba97b44eb9495e
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2025-08-01 22:49:23 +0300

    fix the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Dashbord from './components/layouts/Dashboard/dashboard'
-import Dashboard from '/src/pages/Dashboard/dashboard'
+// import Dashboard from '/src/pages/Dashboard/dashboard'
 import Orders from '/src/pages/Orders/orders'
 import Products from '/src/pages/Products/products'
 import Other from '/src/pages/Other/other'
